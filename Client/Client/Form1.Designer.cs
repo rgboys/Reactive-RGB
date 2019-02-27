284563ee3013fb4ad1f8ae9bdf81633cabd8a768
Joshua Nishiguchi <jsn9@hawaii.edu>
2019-02-26 19:31:14 -1000

    Client - Grabbing monitors and preview

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.combo_monitor = new System.Windows.Forms.ComboBox();
+            this.link_previewMonitor = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Monitor";
+            // 
+            // combo_monitor
+            // 
+            this.combo_monitor.FormattingEnabled = true;
+            this.combo_monitor.Location = new System.Drawing.Point(93, 12);
+            this.combo_monitor.Name = "combo_monitor";
+            this.combo_monitor.Size = new System.Drawing.Size(202, 21);
+            this.combo_monitor.TabIndex = 1;
+            this.combo_monitor.SelectedIndexChanged += new System.EventHandler(this.combo_monitor_SelectedIndexChanged);
+            // 
+            // link_previewMonitor
+            // 
+            this.link_previewMonitor.AutoSize = true;
+            this.link_previewMonitor.Location = new System.Drawing.Point(212, 36);
+            this.link_previewMonitor.Name = "link_previewMonitor";
+            this.link_previewMonitor.Size = new System.Drawing.Size(83, 13);
+            this.link_previewMonitor.TabIndex = 2;
+            this.link_previewMonitor.TabStop = true;
+            this.link_previewMonitor.Text = "Preview Monitor";
+            this.link_previewMonitor.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link_previewMonitor_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.link_previewMonitor);
+            this.Controls.Add(this.combo_monitor);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "RGBoys";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox combo_monitor;
+        private System.Windows.Forms.LinkLabel link_previewMonitor;
+    }
+}
+
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        private Screen prevScreen;
+        private Screen[] screens;
+        public Form1()
+        {
+            InitializeComponent();
+            screens = Screen.AllScreens;
+            int i = 0;
+            foreach (var screen in screens)
+            {
+                string text = string.Format("{0}: {1}{2}", ++i, screen.DeviceName, (screen.Primary) ? " - Primary" : "");
+                combo_monitor.Items.Add(text);
+            }
+            prevScreen = screens[0];
+            combo_monitor.SelectedIndex = 0;
+        }
+
+        private void link_previewMonitor_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Bitmap memoryImage;
+            Graphics myGraphics = this.CreateGraphics();
+            Size s = prevScreen.Bounds.Size;
+            memoryImage = new Bitmap(prevScreen.Bounds.Width, prevScreen.Bounds.Height, myGraphics);
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+            memoryGraphics.CopyFromScreen(prevScreen.Bounds.Left, prevScreen.Bounds.Top, prevScreen.Bounds.Right, prevScreen.Bounds.Bottom, prevScreen.Bounds.Size);
+            
+        }
+
+        private void combo_monitor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            prevScreen = screens[combo_monitor.SelectedIndex];
+        }
+    }
+}
