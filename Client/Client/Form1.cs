4a13c1c6657a4831c23ff7c0a866ba9e7ff3a652
Joshua Nishiguchi <jsn9@hawaii.edu>
2019-03-04 22:12:54 -1000

    Modification to client and clientcapture 'preview'
    The mss monitors and iteration of them manually will incur different results. Thus, I need to grab the preview from mss

--- Client/Client/Form1.cs
+++ Client/Client/Form1.cs
@@ -29,40 +29,40 @@ namespace Client
             int i = 0;
             foreach (var screen in screens)
             {
-                string text = string.Format("{0}: {1}{2}", ++i, screen.DeviceName, (screen.Primary) ? " - Primary" : "");
+                string text = string.Format("{0}: {1}", ++i, screen.DeviceName);
                 combo_monitor.Items.Add(text);
             }
             prevScreen = screens[0];
             combo_monitor.SelectedIndex = 0;
         }
 
+        //Executes python script to grab a preview of the monitor that will be worked with
         private void link_previewMonitor_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            Bitmap memoryImage;
-            Graphics myGraphics = this.CreateGraphics();
-            Size s = prevScreen.Bounds.Size;
-            memoryImage = new Bitmap(prevScreen.Bounds.Width, prevScreen.Bounds.Height, myGraphics);
-            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
-            memoryGraphics.CopyFromScreen(prevScreen.Bounds.Left, prevScreen.Bounds.Top, 0,0, prevScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            var psi = new ProcessStartInfo();
+            psi.FileName = @"C:\Users\lichc\AppData\Local\Programs\Python\Python37\python.exe";
 
-            Console.WriteLine(Path.GetTempPath());
-            string path = @"" + Path.GetTempPath() + "\\RGBoys\\Client\\temp.png";
-            
-            using (MemoryStream memory = new MemoryStream())
+            var script = @"C:\Users\lichc\Desktop\ReactiveRGB\Reactive-RGB\Python\ClientCapture\clientcapture.py";
+            int monitorIndex = combo_monitor.SelectedIndex+1;
+
+            psi.Arguments = $"\"{script}\" \"{"0"}\" \"{monitorIndex}\"";
+
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            var errors = "";
+            var results = "";
+
+            using (var process = Process.Start(psi))
             {
-                using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.ReadWrite))
-                {
-                    memoryImage.Save(memory, ImageFormat.Jpeg);
-                    byte[] bytes = memory.ToArray();
-                    fs.Write(bytes, 0, bytes.Length);
-                }
+                errors = process.StandardError.ReadToEnd();
+                results = process.StandardOutput.ReadToEnd();
             }
-            
 
-            Process photoViewer = new Process();
-            photoViewer.StartInfo.FileName = path;
-            photoViewer.StartInfo.Arguments = path;
-            photoViewer.Start();
+            Console.WriteLine(errors);
+            Console.WriteLine(results);
         }
 
         private void combo_monitor_SelectedIndexChanged(object sender, EventArgs e)
@@ -82,7 +82,7 @@ namespace Client
             int width = 200;
             int height = 200;
             int monitor = combo_monitor.SelectedIndex;
-            psi.Arguments = $"\"{script}\" \"{startX}\" \"{startY}\" \"{width}\" \"{height}\" \"{monitor}\"";
+            psi.Arguments = $"\"{script}\" \"{"1"}\" \"{startX}\" \"{startY}\" \"{width}\" \"{height}\" \"{monitor}\"";
 
             psi.UseShellExecute = false;
             psi.CreateNoWindow = true;
